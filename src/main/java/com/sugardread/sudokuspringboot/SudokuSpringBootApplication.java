d9951fe11c8a457baad2ab91cf62d3c20a9f7dad
sugardread <sugardread@users.noreply.github.com>
2025-09-18 18:19:22 +0300

    Initial commit

---
+++ src/main/java/com/sugardread/sudokuspringboot/SudokuSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sugardread.sudokuspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokuSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SudokuSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sugardread/sudokuspringboot/SudokuSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sugardread.sudokuspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
